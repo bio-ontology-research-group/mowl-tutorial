9a3c279d10b60e6feb5b827f8177972e5d98e28b
Fernando Zhapa-Camacho <ferzcam@gmail.com>
2023-02-05 01:03:18 +0300

    :art:

--- notebooks/scripts/utils.py
+++ notebooks/scripts/utils.py
@@ -17,3 +17,14 @@ def pyvis_network(edges):
         net.add_edge(nodes[src], nodes[dst])
 
     return net
+
+def nx_network(edges):
+    import networkx as nx
+    import matplotlib.pyplot as plt
+    G = nx.DiGraph()
+    for edge in edges:
+        src = edge.src.split("/")[-1]
+        dst = edge.dst.split("/")[-1]
+        G.add_edge(src, dst)
+    nx.draw(G, with_labels=True)
+    plt.show()
